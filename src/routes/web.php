3d99d92af7e429299ff037810617b0379ccce083
Ismar <76652264+ismarapw@users.noreply.github.com>
2022-03-27 12:24:53 +0800

    add-nav

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('marchendise');
 });
 
 Route::get('/daftar', function () {
     return view('daftar');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
