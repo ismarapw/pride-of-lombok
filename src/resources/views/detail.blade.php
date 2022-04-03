1bfae72b21984bb4193af51be8df91158ef827aa
Wana Ardilah Iwan <wanaardilah@student.telkomuniversity.ac.id>
2022-04-03 21:49:01 +0800

    Hasil Kerja Wana

---
+++ src/resources/views/detail.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Desktop 3
+        </title>
+
+        <link rel="stylesheet" href="dekstop3.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        
+    </head>    
+
+    <body>
+
+        <button 
+            class="button" type="submit">+ Beli Merchandise
+        </button>
+
+        <div class="row justify-content-md-center">
+            <img src="./aset/kaintenunsasak.png" style="display:block; margin:auto;" >
+        </div>
+        
+        <h2>
+            Kain Tenun Sasak
+        </h2>
+
+        <h5>
+            Deskripsi
+        </h5>
+
+        <h4>
+            Kain Tenun
+        </h4>
+
+        <h3>
+            Rp125.000
+        </h3>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis eros sem. Vivamus nec massa ac lacus placerat venenatis
+            vel eget tellus. Ut tortor augue, sagittis ac bibendum a, viverra eu magna. Fusce eget mauris sed purus viverra lacinia vitae
+            sit amet odio. Aliquam erat volutpat. Nunc et ligula quam. Proin sagittis mauris sit amet massa dictum, et lacinia nibh ultricies. 
+            In et laoreet dui, in interdum tellus. Curabitur elementum ullamcorper dui, non malesuada libero hendrerit nec. Nulla vel egestas dolor. 
+            Nulla ac consequat mi, sed vehicula sapien. Aliquam tincidunt velit vel molestie lacinia.
+        </p>
+
+
+    </body>
+
+</html>
