d41b0e369dd3616dadf47a02011b9b1514d81394
ismarapw <ismarapw0212@gmail.com>
2023-07-24 22:01:07 +0800

    update-laravel

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -74,6 +74,126 @@ class DatabaseSeeder extends Seeder
             "harga" => 275000,
             "gambar" =>"kendi_maling.jpg"
         ]);
+
+        Marchendise::create([
+            "nama" => "Songket Sasak",
+            "jenis" => "Kain Tenun",
+            "deskripsi" => "Songket adalah kain tenun khas Lombok yang indah dan dihiasi dengan motif tradisional yang rumit. Songket biasanya digunakan untuk pakaian adat dan acara-acara istimewa.",
+            "harga" => 500000,
+            "gambar" =>"songet_sasak_1.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Ikat Lombok",
+            "jenis" => "Kain Tenun",
+            "deskripsi" => "Ikat Lombok adalah kain tenun dengan motif unik yang dihasilkan dari teknik ikat benang sebelum dijadikan kain. Ikat Lombok terkenal karena keindahannya.",
+            "harga" => 400000,
+            "gambar" =>"ikat_lombok.jpg"
+        ]);
+        
+        Marchendise::create([
+            "nama" => "Sasak Pottery",
+            "jenis" => "Kerajinan Gerabah",
+            "deskripsi" => "Kerajinan gerabah khas Lombok yang indah dan beragam. Dari vas bunga hingga barang rumah tangga lainnya, kerajinan gerabah Lombok menjadi pilihan yang menarik sebagai oleh-oleh.",
+            "harga" => 150000,
+            "gambar" =>"sasak_pottery.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Sarung Tenun",
+            "jenis" => "Pakaian",
+            "deskripsi" => "Sarung tenun adalah pakaian tradisional Lombok yang terbuat dari kain tenun yang indah dan berwarna-warni. Sarung tenun sering digunakan dalam acara adat dan perayaan.",
+            "harga" => 250000,
+            "gambar" =>"tenun_lombok.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Gula Aren",
+            "jenis" => "Makanan",
+            "deskripsi" => "Gula aren adalah pemanis alami yang dihasilkan dari nira pohon aren. Rasanya manis dan lezat, cocok untuk dicampurkan dalam minuman atau makanan.",
+            "harga" => 75000,
+            "gambar" =>"gula_aren.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Ampenan Cake",
+            "jenis" => "Makanan",
+            "deskripsi" => "Ampenan Cake adalah kue tradisional Lombok yang lezat dan kenyal. Rasanya manis dengan aroma pandan yang khas.",
+            "harga" => 30000,
+            "gambar" =>"ampenan_kue.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Keripik Rumput Laut",
+            "jenis" => "Makanan",
+            "deskripsi" => "Keripik rumput laut adalah camilan khas Lombok yang renyah dan gurih. Dibuat dari rumput laut pilihan, keripik ini menjadi oleh-oleh yang lezat.",
+            "harga" => 50000,
+            "gambar" =>"kripik_laut.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Topeng Sasak",
+            "jenis" => "Kerajinan",
+            "deskripsi" => "Topeng Sasak adalah kerajinan tangan dari kayu yang menggambarkan karakter-karakter tradisional Lombok. Topeng ini sering digunakan dalam tarian dan upacara adat.",
+            "harga" => 120000,
+            "gambar" =>"topeng_sasak.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Brem Lombok",
+            "jenis" => "Makanan",
+            "deskripsi" => "Brem Lombok adalah kue tradisional yang terbuat dari tape ketan dengan rasa manis dan khas. Cocok sebagai oleh-oleh atau camilan.",
+            "harga" => 45000,
+            "gambar" =>"brem_lombok.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Selong Belanak Painting",
+            "jenis" => "Seni Lukis",
+            "deskripsi" => "Lukisan Selong Belanak menggambarkan keindahan pantai Selong Belanak yang indah. Lukisan ini menjadi kenangan yang indah dari liburan di Lombok.",
+            "harga" => 200000,
+            "gambar" =>"selong_art.jpeg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Banyumulek Pottery",
+            "jenis" => "Kerajinan Gerabah",
+            "deskripsi" => "Banyumulek Pottery adalah kerajinan gerabah dari desa Banyumulek. Barang-barang kerajinan ini unik dan berkualitas tinggi.",
+            "harga" => 180000,
+            "gambar" =>"Banyumulek Pottery.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Perang Topat",
+            "jenis" => "Makanan",
+            "deskripsi" => "Perang Topat adalah makanan tradisional Lombok yang terbuat dari ketan dan kelapa parut. Biasanya disajikan dalam acara perayaan.",
+            "harga" => 30000,
+            "gambar" =>"topat.jpeg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Kain Endek",
+            "jenis" => "Kain Tenun",
+            "deskripsi" => "Endek Lombok adalah kain tenun dengan motif khas dan warna-warna cerah. Kain ini sering digunakan untuk pakaian adat.",
+            "harga" => 350000,
+            "gambar" =>"kain_endek.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Sate Rembiga",
+            "jenis" => "Makanan",
+            "deskripsi" => "Sate Rembiga adalah makanan khas Lombok berupa sate ayam dengan bumbu kacang yang khas. Rasanya lezat dan menggugah selera.",
+            "harga" => 40000,
+            "gambar" =>"sate_rembiga.jpg"
+        ]);
+
+        Marchendise::create([
+            "nama" => "Lombok Coffee",
+            "jenis" => "Minuman",
+            "deskripsi" => "Kopi Lombok terkenal dengan cita rasa yang khas. Biji kopi dipetik dan diproses secara tradisional, menghasilkan kopi berkualitas tinggi.",
+            "harga" => 80000,
+            "gambar" =>"lombok_coffe.jpg"
+        ]);
         
     }
 }
